82c9cd7bb600b3094f81b3704322de2075dec646
BielStela <biel.stela@gmail.com>
2018-11-12 12:52:42 +0100

    pep8

--- potyspoty/comparisson.py
+++ potyspoty/comparisson.py
@@ -17,17 +17,18 @@ import umap
 
 from .features import SongFeatures
 
+
 class Comparator():
 
-    def __init__(self, scaler:str=None, projector:str='pca', plot_engine='plotly'):
+    def __init__(self, scaler: str = None, projector: str = 'pca', plot_engine='plotly'):
         self.scaler = scaler
         self.projector = projector
         self._check_projector(self.projector)
         self._check_scaler(self.scaler)
         self.plot_engine = plot_engine
         self.fited_ = False
-        
-    def _check_scaler(self, scaler:str) -> None:
+
+    def _check_scaler(self, scaler: str) -> None:
         """
         Safety param check 
         """
@@ -39,8 +40,8 @@ class Comparator():
         else:
             raise ValueError('scaler value must be "minmax",'
                              ' "standard" or None')
-    
-    def _check_projector(self, projector:str) -> None:
+
+    def _check_projector(self, projector: str) -> None:
         """
         Safety param check 
         """
@@ -54,32 +55,32 @@ class Comparator():
         self.n_bands = len(self.bands_names)
         self.audio_features = bands_features[0].features
 
-        self.features_all, self.band_names_long = self.concat_(self.bands_features)
-        
+        self.features_all, self.band_names_long = self.concat_(
+            self.bands_features)
+
         if self.scaler is not None:
             if self.scaler == 'minmax':
                 scl = MinMaxScaler()
-            
+
             elif self.scaler == 'standard':
                 scl = StandardScaler()
 
-            
             self.features_all = scl.fit_transform(self.features_all)
-        
+
         if self.projector == 'pca':
             projector_ = PCA(n_components=2)
-        
+
         elif self.projector == 'umap':
             projector_ = umap.UMAP()
-        
+
         elif self.projector == 'tsne':
             projector_ = TSNE()
-        
+
         self.projected_features = projector_.fit_transform(self.features_all)
 
         if self.plot_engine == 'plotly':
             self.plot_plotly()
-        
+
         else:
             self.plot_()
         self.fited_ = True
@@ -95,46 +96,49 @@ class Comparator():
         return df_all, band_names
 
     def plot_(self):
-        sns.scatterplot(self.projected_features[:,0],
-                        self.projected_features[:,1],
+        sns.scatterplot(self.projected_features[:, 0],
+                        self.projected_features[:, 1],
                         hue=self.band_names_long,
                         style=self.band_names_long)
-    
+
     def plot_plotly(self):
         palette = sns.palettes.color_palette('Set1', self.n_bands)
         traces = []
         for i, df in enumerate(self.bands_features):
             trace0 = go.Scatter(
-                x = self.projected_features[np.array(self.band_names_long)==df.band_name, 0],
-                y = self.projected_features[np.array(self.band_names_long)==df.band_name, 1],
-                mode= 'markers',
-                marker= dict(size= 8,
-                            color= 'rgb' + str(palette[i]),
-                            opacity= 0.8),
-                text = df.name,
-                name = df.band_name
-                )
+                x=self.projected_features[np.array(
+                    self.band_names_long) == df.band_name, 0],
+                y=self.projected_features[np.array(
+                    self.band_names_long) == df.band_name, 1],
+                mode='markers',
+                marker=dict(size=8,
+                            color='rgb' + str(palette[i]),
+                            opacity=0.8),
+                text=df.name,
+                name=df.band_name
+            )
             traces.append(trace0)
-    
+
         layout = go.Layout(
-            hovermode= 'closest',
+            hovermode='closest',
             autosize=False,
             width=1000,
             height=700)
-    
-        fig= go.Figure(data=traces, layout=layout)
+
+        fig = go.Figure(data=traces, layout=layout)
         iplot(fig)
 
 
-def most_similar_song(band1:SongFeatures, band2:SongFeatures) -> pd.DataFrame:
+def most_similar_song(band1: SongFeatures,
+                      band2: SongFeatures) -> pd.DataFrame:
 
-    band1_feat = band1.loc[:,band1.features]
-    band2_feat =  band2.loc[:,band2.features]
+    band1_feat = band1.loc[:, band1.features]
+    band2_feat = band2.loc[:, band2.features]
     closests_idx = pairwise_distances_argmin(band1_feat, band2_feat)
-    closest_songs = band2.data_frame.iloc[closests_idx,:]['name'].values
+    closest_songs = band2.data_frame.iloc[closests_idx, :]['name'].values
 
     df = pd.DataFrame({
-        'name': band1.loc[:,'name'],
+        'name': band1.loc[:, 'name'],
         'closest': closest_songs
-        })
+    })
     return df
